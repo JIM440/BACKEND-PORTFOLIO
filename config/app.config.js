e51910168ef14c265f30c7a18f090b27e9257b3b
nibaverine <nibaverinekajock@gmail.com>
2023-06-06 08:48:03 +0100

    backend_complete

---
+++ config/app.config.js
@@ -0,0 +1,9 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+    DB: 'mongodb+srv://takemjim43:jQDpFPDi6aSnJF6S@reactbackendportfolio.qkpd8v8.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
